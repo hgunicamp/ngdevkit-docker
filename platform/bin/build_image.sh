6aba1ef9d49c9f6f1753452c7aff896ff761b7fa
Honório Gomes <hgunicamp@gmail.com>
2022-04-09 18:39:10 -0300

    Inserting the docker-compose.yaml file.

--- platform/bin/build_image.sh
+++ platform/bin/build_image.sh
@@ -2,8 +2,8 @@
 
 # Default variables.
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-NG_DEVKIT_DIR=${SCRIPT_DIR}/../ngdevkit-server
+NG_DEVKIT_BASE_DIR=${SCRIPT_DIR}/../
 
 # Changing folder to start the building process.
-cd ${NG_DEVKIT_DIR}
+cd ${NG_DEVKIT_BASE_DIR}
 docker-compose build
