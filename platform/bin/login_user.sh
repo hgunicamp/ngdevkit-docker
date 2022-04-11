f7a6292096d543f477b0564bb790d85e0a4248a0
Honório Gomes <hgunicamp@gmail.com>
2022-04-10 23:57:24 -0300

    Allowing the joystick and to be accessed inside the container.

--- platform/bin/login_user.sh
+++ platform/bin/login_user.sh
@@ -4,6 +4,8 @@
 # Default variables.
 LOGIN_SHELL="${1:-bash}"
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+CURRENT_USER_ID=$(id -u)
+GROUP_INPUT=$(grep input /etc/group | cut -d':' -f3)
 
 # Allowing X11 connection.
 xhost +local:${USERNAME}
@@ -11,9 +13,9 @@ xhost +local:${USERNAME}
 # Starting a new container.
 docker run \
   --rm \
-  --device /dev/dri:/dev/dri \
-  --device /dev/input:/dev/input \
-  -u $(id -u):$(id -g) \
+  --device /dev/dri \
+  --device /dev/input \
+  -u ${CURRENT_USER_ID}:${GROUP_INPUT} \
   -e DISPLAY=${DISPLAY} \
   -e XDG_RUNTIME_DIR=${XDG_RUNTIME_DIR} \
   -e HOME=/home/remote \
